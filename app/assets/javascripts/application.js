6539d1e0982f10b4748ff1d2b9f272870b38f8a5
Matthew Vilhauer <matthewvilhauer@gmail.com>
2015-06-09 18:44:24 -0400

    Installed nested form gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery_nested_form
 
 function remove_fields(link) {
   $(link).previous("input[type=hidden]").value = "1";
